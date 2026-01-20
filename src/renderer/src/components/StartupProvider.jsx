d4f200564d5b4bc3b8a1e88573b530c3f80e151e
Stefano <30950154+Stefifox@users.noreply.github.com>
2026-01-20 21:48:55 +0100

    feat: added loading page

--- src/renderer/src/components/StartupProvider.jsx
+++ src/renderer/src/components/StartupProvider.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
+import { CircularProgress, Typography } from '@mui/material'
 
 import { useDatabase } from '../hooks'
 import { setInitialized, setSettings } from '../slices/appSlice'
@@ -7,28 +9,46 @@ import { setInitialized, setSettings } from '../slices/appSlice'
 export default function StartupProvider({ children }) {
   const dispatch = useDispatch()
   const db = useDatabase('tab_settings')
+  const { i18n } = useTranslation()
 
   const [isLoading, setLoading] = React.useState(true)
+  const [initialized, setInit] = React.useState(false)
 
   const loadData = (data) => {
     const obj = {}
     data.forEach((val) => {
-      obj[val.sett_key] = val.sett_value
+      if (val.settKey === 'initState') {
+        setInit(val.sett_value === '1')
+      } else {
+        obj[val.sett_key] = val.sett_value
+      }
     })
     return obj
   }
 
   React.useEffect(() => {
-    db.getData({ columns: ['*'] }).then((data) => {
-      const appData = loadData(data)
-      dispatch(setSettings(appData))
-      setLoading(false)
-    })
+    db.getData({ columns: ['*'] })
+      .then((data) => {
+        const appData = loadData(data)
+        dispatch(setSettings(appData))
+        dispatch(setInitialized(initialized))
+        i18n.changeLanguage(appData.language)
+        console.log("App Settings: ", appData)
+        setLoading(false)
+      })
+      .catch(() => {
+        dispatch(setInitialized(false))
+        setLoading(false)
+      })
   }, [])
 
   if (isLoading) {
-    return <></>
+    return (
+      <div className="loadScreen">
+        <CircularProgress />
+        <Typography variant="h6">Loading...</Typography>
+      </div>
+    )
   }
-
   return children
 }
