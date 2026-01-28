b02145c9b9cd0e02283eccf02b85e219308cb70c
Stefano <30950154+Stefifox@users.noreply.github.com>
2026-01-28 20:52:06 +0100

    feat: created UpdateItemDialog

--- src/renderer/src/components/dialogs/UpdateItemDialog.jsx
+++ src/renderer/src/components/dialogs/UpdateItemDialog.jsx
@@ -6,19 +6,19 @@ import { showError, showSuccess } from '../../slices/messageSlice'
 import editors from '../Editors/editors'
 import { useState } from 'react'
 
-export default function showAddItemDialog(props) {
+export default function showUpdateItemDialog(props) {
   console.log(props)
-  props.dispatch(showDialog({ content: <AddItemDialog {...props} />, maxWidth: 'md' }))
+  props.dispatch(showDialog({ content: <UpdateItemDialog {...props} />, maxWidth: 'md' }))
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
-function AddItemDialog(props) {
+function UpdateItemDialog(props) {
   const { t } = useTranslation()
 
-  const { dispatch, database, reload, type } = props
+  const { dispatch, database, reload, type, keyColumn, defaultModel } = props
   const Editor = editors[type]
 
-  const [model, setModel] = useState({})
+  const [model, setModel] = useState(defaultModel || {})
 
   const updateModel = ({ target }) => {
     const { name, value } = target
@@ -29,8 +29,15 @@ function AddItemDialog(props) {
   }
 
   const saveClick = () => {
+    console.log({
+      values: model,
+      where: { column: keyColumn, type: 'equals', value: model[keyColumn] }
+    })
     database
-      .addData(model)
+      .updData({
+        values: model,
+        where: [{ column: keyColumn, type: 'equals', value: model[keyColumn] }]
+      })
       .then(() => {
         reload()
         dispatch(showSuccess)
@@ -43,9 +50,9 @@ function AddItemDialog(props) {
 
   return (
     <>
-      <DialogTitle>{t('DIALOG_ADD')}</DialogTitle>
+      <DialogTitle>{t('DIALOG_UPDATE')}</DialogTitle>
       <DialogContent>
-        <Typography variant="h6">{t('DIALOG_ADD_TEXT')}</Typography>
+        <Typography variant="h6">{t('DIALOG_UPDATE_TEXT')}</Typography>
         <Editor {...props} model={model} updateModel={updateModel} />
       </DialogContent>
       <DialogActions>
